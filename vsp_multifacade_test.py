3ea98e0a7f9aa07332483ce3aab9ffe4fbc9533e
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2025-02-06 09:10:02 -0500

    Adding example using new multi-facade interface

---
+++ vsp_multifacade_test.py
@@ -0,0 +1,41 @@
+import openvsp_config
+openvsp_config.LOAD_GRAPHICS = True
+openvsp_config.LOAD_MULTI_FACADE = True
+
+import openvsp as vsp
+import degen_geom as dg
+
+# VSP file containing single
+vsp_file = "rect_wing.vsp3"
+
+# Create a private vsp server instance
+vsp_model = vsp.vsp_servers.start_vsp_instance("openaerostruct")
+
+# Read in file
+vsp_model.ReadVSPFile(vsp_file)
+
+# Create a degengeom set that will have our VLM surfaces in it
+vsp_model.SetAnalysisInputDefaults("DegenGeom")
+vsp_model.SetIntAnalysisInput("DegenGeom", "WriteCSVFlag", [0], 0)
+vsp_model.SetIntAnalysisInput("DegenGeom", "WriteMFileFlag", [0], 0)
+
+# Export all degengeoms to a list
+degen_results_id = vsp_model.ExecAnalysis("DegenGeom")
+
+# Get all of the degen geom results managers ids
+degen_ids = vsp_model.GetStringResults(degen_results_id, "Degen_DegenGeoms")
+
+# Create a list of all degen surfaces
+degens = []
+# loop over all degen objects
+for degen_id in degen_ids:
+    res = vsp_model.parse_results_object(degen_id)
+    degen_obj = dg.DegenGeom(res)
+
+    # Create a degengeom object for the cambersurface
+    plate_ids = vsp_model.GetStringResults(degen_id, "plates")
+    for plate_id in plate_ids:
+        res = vsp_model.parse_results_object(plate_id)
+        degen_obj.plates.append(dg.DegenPlate(res))
+
+    degens.append(degen_obj)
